d66acfbfd3d271fba8cfb35e2708e8d6e61e9c50
ilwel <ilwelisaac@hotmail.com>
2021-11-28 05:29:24 -0300

    cors new origin fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = new Koa();
 const server = http.createServer(app.callback());
 const io = socket(server, {
   cors: {
-    origins: ["http://localhost:3000", "https://chat-socket-io-front.netlify.app"],
+    origin: "https://chat-socket-io-front.netlify.app",
     methods: ["GET", "POST"]
   }
 });
